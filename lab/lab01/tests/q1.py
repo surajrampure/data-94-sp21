da08328afcf18e19985c6ff1463113758d36dfea
Isaac Merritt <isaacmerritt@berkeley.edu>
2021-01-29 14:17:40 -0800

    lab01 fux

--- lab/lab01/tests/q1.py
+++ lab/lab01/tests/q1.py
@@ -1,6 +1,6 @@
 test = {   'name': 'q1',
     'points': 1,
-    'suites': [   {   'cases': [{'code': '>>> isinstance(sa, float)\n', 'hidden': False, 'locked': False}, {'code': '>>> sa == 1256.0\n', 'hidden': False, 'locked': False}],
+    'suites': [   {   'cases': [{'code': '>>> isinstance(sa, float)\nTrue', 'hidden': False, 'locked': False}, {'code': '>>> sa == 1256.0\nTrue', 'hidden': False, 'locked': False}],
                       'scored': True,
                       'setup': '',
                       'teardown': '',
