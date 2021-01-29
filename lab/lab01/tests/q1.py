586d32375ed400fb70a5c0a9e2259e2fd6f83fb7
Isaac Merritt <isaacmerritt@berkeley.edu>
2021-01-28 20:04:11 -0800

    lab01 upload

---
+++ lab/lab01/tests/q1.py
@@ -0,0 +1,7 @@
+test = {   'name': 'q1',
+    'points': 1,
+    'suites': [   {   'cases': [{'code': '>>> isinstance(sa, float)\n', 'hidden': False, 'locked': False}, {'code': '>>> sa == 1256.0\n', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
