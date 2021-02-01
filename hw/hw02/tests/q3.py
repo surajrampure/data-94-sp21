d611535ff4ce7b71995678014d3f7a7f93aa01ba
Isaac Merritt <isaacmerritt@berkeley.edu>
2021-01-31 22:48:28 -0800

    maintenence

--- hw/hw02/tests/q3.py
+++
@@ -1,9 +0,0 @@
-test = {   'name': 'q3',
-    'points': 3,
-    'suites': [   {   'cases': [   {'code': '>>> hailstone(4)\n4\n2\n1\n3', 'hidden': False, 'locked': False},
-                                   {'code': '>>> hailstone(10)\n10\n5\n16\n8\n4\n2\n1\n7', 'hidden': False, 'locked': False},
-                                   {'code': '>>> hailstone(15)\n15\n46\n23\n70\n35\n106\n53\n160\n80\n40\n20\n10\n5\n16\n8\n4\n2\n1\n18', 'hidden': False, 'locked': False}],
-                      'scored': True,
-                      'setup': '',
-                      'teardown': '',
-                      'type': 'doctest'}]}
