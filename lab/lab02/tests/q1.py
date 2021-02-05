57f175822ea2ace318283ac560628a257f2b6525
Isaac Merritt <isaacmerritt@berkeley.edu>
2021-02-04 21:19:16 -0800

    lab02 upload

---
+++ lab/lab02/tests/q1.py
@@ -0,0 +1,9 @@
+test = {   'name': 'q1',
+    'points': 0,
+    'suites': [   {   'cases': [   {'code': '>>> sum_digits(123) == 6\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> sum_digits(0) == 0\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> sum_digits(1234) == 10\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
