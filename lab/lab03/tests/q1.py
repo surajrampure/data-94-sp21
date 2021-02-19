e6e36cb439c594d064fb901b730b12a0829189bd
Isaac Merritt <isaacmerritt@berkeley.edu>
2021-02-18 23:35:08 -0800

    lab03 upload

---
+++ lab/lab03/tests/q1.py
@@ -0,0 +1,8 @@
+test = {   'name': 'q1',
+    'points': 0,
+    'suites': [   {   'cases': [   {'code': '>>> pairwise_multiply([2, 3, 4], [10, 20, 30]) == [20, 60, 120]\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> pairwise_multiply(list(range(1, 10)), list(range(9, 0, -1))) == [9, 16, 21, 24, 25, 24, 21, 16, 9]\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
