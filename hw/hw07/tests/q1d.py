02298a2018067873cdf0309e10001a9d2b9254b7
Isaac Merritt <isaacmerritt@berkeley.edu>
2021-03-31 13:20:24 -0700

    hw07 upload

---
+++ hw/hw07/tests/q1d.py
@@ -0,0 +1,8 @@
+test = {   'name': 'q1d',
+    'points': 1,
+    'suites': [   {   'cases': [   {'code': ">>> species_by_island.labels == ('species', 'Biscoe', 'Dream', 'Torgersen')\nTrue", 'hidden': False, 'locked': False},
+                                   {'code': ">>> np.all(species_by_island.column('Biscoe') == np.array([44, 0, 119]))\nTrue", 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
